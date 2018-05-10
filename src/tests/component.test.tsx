b6ae0539e7567616c5dc6c9766af420b3879e2a2
pancomputationalist <15155173+bearbytes@users.noreply.github.com>
2018-05-10 23:18:14 +0200

    one more test

--- src/tests/component.test.tsx
+++ src/tests/component.test.tsx
@@ -1,8 +1,8 @@
 import {shallow} from 'enzyme'
 import * as React from 'react'
-import {CounterComponent, flux} from './test.fixture'
+import {CounterComponent, flux, increment} from './test.fixture'
 
-test('root component can be created', () => {
+test('basic test', () => {
   const initialState = {count: 42}
   const store = flux.createStore({initialState})
 
@@ -17,4 +17,24 @@ test('root component can be created', () => {
 
   const wrapper = shallow(<MyComponent/>)
   expect(wrapper.html()).toBe('<p>142</p>')
+})
+
+test('component updates on state change', () => {
+  const initialState = {count: 42}
+  const store = flux.createStore({initialState})
+
+  const StateProvider = store.provider
+  const MyComponent = () => {
+    return (
+      <StateProvider>
+        <CounterComponent offsetBy={100}/>
+      </StateProvider>
+    )
+  }
+
+  const wrapper = shallow(<MyComponent/>)
+  expect(wrapper.html()).toBe('<p>142</p>')
+
+  store.dispatch(increment({by: 1}))
+  expect(wrapper.html()).toBe('<p>143</p>')
 })
